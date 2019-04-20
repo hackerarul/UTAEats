6df54fa668ba495695e1f7d95973d803c05872b7
hackerarul <49766879+hackerarul@users.noreply.github.com>
2019-04-20 15:20:27 -0500

    Add files via upload
    package = com.example.utaeats

---
+++ src/HomeDrawerB.java
@@ -0,0 +1,14 @@
+package com.example.utaeats;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeDrawerB extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drawer_buyer);
+
+
+    }
+}
